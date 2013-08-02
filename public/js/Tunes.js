78d24ae93c768c1a8bad51cb34dfb6d2b1ad665f
Gilberto Villa <ingilniero@gmail.com>
2013-08-01 22:16:44 -0500

    Add support to identify first, last, and retrieve a track URL

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -1,6 +1,22 @@
 (function($) {
 
-  window.Album = Backbone.Model.extend({});
+  window.Album = Backbone.Model.extend({
+    isFirstTrack: function(index){
+      return index == 0;
+    },
+
+    isLastTrack: function(index){
+      return index >= this.get('tracks').length - 1;
+    },
+
+    trackUrlAtIndex: function(index) {
+      if (this.get('tracks').length >= index) {
+        return this.get('tracks')[index].url;
+      } else {
+        return null;
+      }
+    }
+  });
 
   window.AlbumView = Backbone.View.extend({
     tag: 'li',
