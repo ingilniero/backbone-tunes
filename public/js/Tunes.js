1b4e91604380ad096fac0d95cdd34f2b091c8e24
Gilberto Villa <ingilniero@gmail.com>
2013-08-01 22:22:06 -0500

    Add Albums collection

--- public/js/Tunes.js
+++ public/js/Tunes.js
@@ -18,6 +18,11 @@
     }
   });
 
+  window.Albums = Backbone.Collection.extend({
+    model: Album,
+    url: '/albums'
+  });
+
   window.AlbumView = Backbone.View.extend({
     tag: 'li',
     className: 'album',
